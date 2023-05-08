f03f802d4492358e8151683f565c010add2c1f49
Tiago Silva <tiago@tiago.blog.br>
2023-05-08 11:45:18 -0300

    Alteracoes no banco de dados

--- App/Controller/ClienteController.php
+++ App/Controller/ClienteController.php
@@ -2,26 +2,11 @@
 
 namespace App\Controller;
 
-/**
- * Definimos aqui que nossa classe precisa incluir uma classe de outro subnamespace
- * do projeto, no caso a classe PessoaModel do subnamespace Model
- */
 use App\Model\ClienteModel;
 
 
-/**
- * Classes Controller são responsáveis por processar as requisições do usuário.
- * Isso significa que toda vez que um usuário chama uma rota, um método (função)
- * de uma classe Controller é chamado.
- * O método poderá devolver uma View (fazendo um include), acessar uma Model (para
- * buscar algo no banco de dados), redirecionar o usuário de rota, ou mesmo,
- * chamar outra Controller.
- */
 class ClienteController extends Controller
 {
-    /**
-     * Preenche um Model para que seja enviado ao banco de dados para salvar.
-     */
     public static function login()
     {
         // Transformando os dados da entrada enviada do app em
@@ -54,7 +39,7 @@ class ClienteController extends Controller
 
        $model->save(); // chamando o método save da model.
 
-       parent::setResponseAsJSON('Reclamação inserida com sucesso!');       
+       //parent::setResponseAsJSON('Reclamação inserida com sucesso!');       
     }
 
     /**
