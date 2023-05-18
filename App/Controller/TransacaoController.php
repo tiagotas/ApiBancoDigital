9a0db4ae543c87c0154315b07e4282c8f993e08b
Usuario <Usuario@CPD02-M00>
2023-05-18 15:58:01 -0300

    Ajustes para transações

--- App/Controller/TransacaoController.php
+++ App/Controller/TransacaoController.php
@@ -21,11 +21,13 @@ class TransacaoController extends Controller
 {
     public static function receberPix()
     {
+        $data = json_decode(file_get_contents('php://input'));
 
     }
 
     public static function enviarPix()
     {
+        $data = json_decode(file_get_contents('php://input'));
 
     }
 }
